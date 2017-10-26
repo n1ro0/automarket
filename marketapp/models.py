8bef139583a57d5aa1106a1190bc5110d706e37a
n1ro0 <n1ro0ilya@gmail.com>
2017-10-26 17:16:38 +0300

    first commit

---
+++ marketapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class Owner(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+
+
+class Owner(models.Model):
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=30)
+
+# Create your models here.
